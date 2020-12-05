84b345fa1dc476422cf990ff1c8119d55d1eb9e9
saurabh <s.khonde49@gmail.com>
2020-12-05 08:51:06 +0530

    Added 2nd Method

--- SkDemo01/src/main/java/com/example/demo/Sdemo01Application.java
+++ SkDemo01/src/main/java/com/example/demo/Sdemo01Application.java
@@ -9,11 +9,17 @@ public class Sdemo01Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Sdemo01Application.class, args);
 		hello();
+		hello2();
 	}
 	
 	public static void hello()
 	{
-		System.out.println("Hello World...!!!");
+		System.out.println("Hello World-1...!!!");
+	}
+	
+	public static void hello2()
+	{
+		System.out.println("Hello World-2..!!!");
 	}
 
 }
